bc3f26d3d35fcd740cd45a705c5fc2b25476d7ea
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-12-14 18:13:53 +0100

    fix typo in ExerciseDetail.js import

--- src/pages/ExersiceDetail.js
+++ src/pages/ExersiceDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParms } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
