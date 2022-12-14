dafa653b33b14a29dc13ef595dc88c836c7cce92
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-12-14 19:13:56 +0100

    add exercise text to detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -14,6 +14,14 @@ const Detail = ({ exerciseDetail }) => {
       sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
     >
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", sx: "20px" } }}>
+        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h6">
+          Exercises keep you strong. {name} {` `}
+          is one of the best exercises to target your {target}. it will help you
+          improve your mood and gain energy.
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
